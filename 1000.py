3bf28efa7b7ef3acc285f11e32b5d832780f17de
Lucas Iuri <lucasiuri@hotmail.com>
2024-01-30 18:47:47 -0300

    Updating the first solution

---
+++ 1000.py
@@ -0,0 +1,5 @@
+# https://www.beecrowd.com.br/judge/en/problems/view/1000
+
+# Your first program in any programming language is usually "Hello World!". In this first problem all you have to do is print this message on the screen. 
+
+print('Hello World!')
